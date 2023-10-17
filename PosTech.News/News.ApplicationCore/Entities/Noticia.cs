e2dba2b7f083053f33903b6eefc77853a738fcab
Eduardo L Gomes <edugomesti@outlook.com>
2023-10-17 17:57:05 -0300

    chore(workflow): segregado workflows

--- PosTech.News/News.ApplicationCore/Entities/Noticia.cs
+++ PosTech.News/News.ApplicationCore/Entities/Noticia.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace News.ApplicationCore.Entities
+﻿namespace News.ApplicationCore.Entities
 {
+    //Create doc
     public class Noticia
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
-        public string Descricao { get; set; }        
+        public string Descricao { get; set; }
         public DateTime DataPublicacao { get; set; }
         public string Autor { get; set; }
     }
